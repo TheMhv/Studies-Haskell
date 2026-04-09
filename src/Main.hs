18849786a9ab235447173ce888cdcffc5d9723df
TheMhv <TheMhv@proton.me>
2026-04-09 14:23:28 -0300

    study(types): first type construct

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,14 @@
 module Main where
 
+data Boolean = F | T -- F = False | T = True
+
+showBool :: Boolean -> String
+showBool T = "True"
+showBool F = "False"
+
 doubleMe :: Int -> Int
 doubleMe x = x + x
 
 main :: IO ()
-main = putStrLn (show (doubleMe 2))
+-- main = putStrLn (show (doubleMe 2))
+main = putStrLn (showBool T)
